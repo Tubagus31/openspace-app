be84e06828e2974d293d764824c55ba218eb0002
Tubagus31 <ajiprasetia79@gmail.com>
2025-05-23 19:09:41 +0700

    test

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -7,7 +7,7 @@ function useInput(defaultValue = "") {
     setValue(target.value);
   }
 
-  return [value, handleValueChange, setValue, val];
+  return [value, handleValueChange, setValue];
 }
 
 export default useInput;
